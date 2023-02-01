1f7b413ded6228e8c9329ea7ba6331fe09c55b54
Hari Tjahjo <haritjahjo@gmail.com>
2023-02-01 23:39:27 +0700

    theming frontend

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,80 @@
+<x-frontend.layout>
+
+<main id="main">
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients section-bg">
+      <div class="container">
+
+        <div class="row" data-aos="zoom-in">
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-1.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-2.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-3.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-4.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-5.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-6.png" class="img-fluid" alt="">
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Cliens Section -->
+
+    <!-- ======= About Us Section ======= -->
+    <x-frontend.about />
+
+    <!-- ======= Why Us Section ======= -->
+    <x-frontend.why-us />
+    <!-- End Why Us Section -->
+
+    <!-- ======= Skills Section ======= -->
+    <x-frontend.skills />
+    <!-- End Skills Section -->
+
+    <!-- ======= Services Section ======= -->
+    <x-frontend.services />
+    <!-- End Services Section -->
+
+    <!-- ======= Cta Section ======= -->
+    <x-frontend.cta />
+    <!-- End Cta Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <x-frontend.portofolio />
+    <!-- End Portfolio Section -->
+
+    <!-- ======= Team Section ======= -->
+    <x-frontend.team />
+    <!-- End Team Section -->
+
+    <!-- ======= Pricing Section ======= -->
+    <x-frontend.pricing />
+    <!-- End Pricing Section -->
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    <x-frontend.faq />
+    <!-- End Frequently Asked Questions Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <x-frontend.contact />
+    <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+</x-frontend.layout>  
